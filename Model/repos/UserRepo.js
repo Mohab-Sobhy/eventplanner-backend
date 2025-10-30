8c8b0496981fdf564a6b1bba417391ed5cb9e353
Fady <fadynagy147@gmail.com>
2025-10-30 18:10:04 +0300

    Registeration validation

--- Model/repos/UserRepo.js
+++ Model/repos/UserRepo.js
@@ -5,6 +5,7 @@ class UserRepo {
     async addUser(username, email, password) {
 
         const hashedPassword = await bcrypt.hash(password, 10);
+
         await client.query(
             `INSERT INTO Users (username,email,password)
                  values ($1,$2,$3)`,
